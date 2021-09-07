d83a71e3f8fca09c2836e5a4945ce24098188083
Juanse1595 <3309@holbertonschool.com>
2021-09-07 03:49:14 +0000

    FIX: 10th changing while for do while

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,14 +10,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *p1 = list, *p2 = (list)->next;
 
-	while (p1 != p2 && p2 != NULL)
-	{
+	do {
 		p1 = p1->next;
 		p2 = p2->next;
 		if (p2 == NULL)
 			return (0);
 		p2 = p2->next;
-	}
+	} while (p1 != p2 && p2 != NULL);
 	if (p1 == p2)
 		return (1);
 	else
