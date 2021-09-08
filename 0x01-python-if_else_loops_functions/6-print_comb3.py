5b2c050facdc0ea602022738e201b090304b35eb
Juanse1595 <3309@holbertonschool.com>
2021-09-08 00:22:53 +0000

    ADD: 6th program

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digit1 in range(0, 10):
+    for digit2 in range(0, 10):
+        if digit1 != digit2 and digit2 > digit1 and (digit1 != 8 and
+                                                     digit2 != 9):
+            print("{}{}".format(digit1, digit2), end=", ")
+print(89)
