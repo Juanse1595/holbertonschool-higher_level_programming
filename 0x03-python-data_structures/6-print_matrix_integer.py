edcd8a335bcdcc1012509ebc19dac623b459cbbd
Juanse1595 <3309@holbertonschool.com>
2021-09-14 01:37:39 +0000

    FIX: conditions

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
+        count = 0
         for row in matrix:
             for num in row:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(num), end="")
+                if count < (len(row) - 1):
+                    print(" ", end="")
+                count += 1
             print()
+            count = 0
