956be230cafb05c0b16cd6c501c4733d2e307cd0
Juanse1595 <3309@holbertonschool.com>
2021-09-14 00:44:39 +0000

    FIX: pep8

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is not None: 
+    if my_string is not None:
         new_string = ""
         for letter in my_string:
             if letter != 'c' and letter != 'C':
