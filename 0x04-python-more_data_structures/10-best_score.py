452074f4649522855db9300e2b58a495c2ea7748
Juanse1595 <3309@holbertonschool.com>
2021-09-16 01:18:27 +0000

    Test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary == None:
         return None
     max_value = a_dictionary[list(a_dictionary.keys())[0]]
     for key in a_dictionary:
