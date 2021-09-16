5265b5c1ea98bb91cabcc561f5c3f95b4b150a93
Juanse1595 <3309@holbertonschool.com>
2021-09-16 01:19:38 +0000

    FIX: 10th condition is now >= max_value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == {}:
         return None
     max_value = a_dictionary[list(a_dictionary.keys())[0]]
     for key in a_dictionary:
-        if a_dictionary[key] > max_value:
+        if a_dictionary[key] >= max_value:
             answer = key
             max_value = a_dictionary[key]
     return answer
