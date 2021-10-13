c93398fe5c959dfcdeaa1f5985b0c71f02a50bbd
Juanse1595 <3309@holbertonschool.com>
2021-10-13 02:37:54 +0000

    FIX pycodestyle 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,7 +12,6 @@ class Student:
         __init__
         to_json
     '''
-    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -20,4 +19,3 @@ class Student:
 
     def to_json(self):
         return self.__dict__
-
