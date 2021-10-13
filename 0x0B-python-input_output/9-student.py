c20f4f4be70f5462a3994cc856a9544154ad5e93
Juanse1595 <3309@holbertonschool.com>
2021-10-13 02:36:39 +0000

    ADD 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''9-student module'''
+
+
+class Student:
+    '''
+    Attr:
+        first_name
+        last_name
+        age
+    Methods:
+        __init__
+        to_json
+    '''
+    
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
