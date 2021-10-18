141c7bc3fef538977874b261d2523ba0bedc0ad4
Juanse1595 <3309@holbertonschool.com>
2021-10-18 21:03:00 +0000

    Update base save_to_file method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''instantation of object'''
         if id is not None:
             self.id = id
         else:
@@ -30,6 +31,22 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        '''return json representation of list_dictionaries'''
         if list_dictionaries is None or list_dictionaries is {}:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        save in a file the json 
+        representation of list_objs
+        '''
+        if list_objs is None:
+            list_objs = []
+        instances_dict = []
+        for instance in list_objs:
+            instances_dict.append((instance.to_dictionary()).copy())
+        json_dict = Base.to_json_string(instances_dict)
+        with open('{}.json'.format(cls.__name__), mode='w') as file_1:
+            file_1.write(json_dict)
