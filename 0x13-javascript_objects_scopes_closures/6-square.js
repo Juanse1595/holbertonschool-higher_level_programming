bc35e10dc490e8a53c470c1007ded7bb72d16698
Juanse1595 <3309@holbertonschool.com>
2021-12-14 23:24:27 +0000

    FIX 6th file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-const Squareparent = require('./5-square');
+/** Define Rectangle class */
+const Square1 = require('./5-square.js');
 
-module.exports = class Square extends Squareparent {
-  charPrint (c) {
-    let block;
-    if (!c) {
-      block = 'X';
-    } else {
+module.exports = class Square extends Square1 {
+  charPrint(c) {
+    let block = 'X';
+    if (c) {
       block = c;
     }
-    for (let i = 0; i < this.heigth; i++) {
+    for (let i = 0; i < this.height; i++) {
       console.log(block.repeat(this.width));
     }
   }
