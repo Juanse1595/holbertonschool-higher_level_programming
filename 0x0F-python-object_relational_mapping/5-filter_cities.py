51f23c14fd043935b408e0ac830685ad51258166
Juanse1595 <3309@holbertonschool.com>
2021-12-19 15:46:02 +0000

    fix 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT name \
-                FROM cities \
-                WHERE state_id = \
-                (SELECT id \
-                FROM states \
+    cur.execute("""SELECT name
+                FROM cities
+                WHERE state_id =
+                (SELECT id
+                FROM states
                 WHERE name LIKE BINARY %s)
-                ORDER BY id ASC", (sys.argv[4]))
+                ORDER BY id ASC""", (sys.argv[4],))
     query_rows = cur.fetchall()
     print(', '.join(row[0] for row in query_rows))
     cur.close()
