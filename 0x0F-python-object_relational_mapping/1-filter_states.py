61d3dd5d2e99b19aeb905845f57da8593c8c929a
Juanse1595 <3309@holbertonschool.com>
2021-12-19 14:59:43 +0000

    imports inside if cond

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''1-filter_states module'''
-import sys
-import MySQLdb
 
 
 if __name__ == '__main__':
+	import sys
+	import MySQLdb
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
