93fe5aaa05d4605078bb4e07e59f0438d0ee57f1
Juanse1595 <3309@holbertonschool.com>
2022-01-08 17:30:42 +0000

    returning it to nomral

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to that URL and displays the size
-curl -sI "$1" | grep Content-Length | cut -d " " -f 2
+curl -o /dev/null -s -w "%{size_download}\n" "$1"
