8920436e5e1c3beb18ef49811f18dda9bfcdd579
Juanse1595 <3309@holbertonschool.com>
2022-01-08 20:30:02 +0000

    FIX 6 conditional

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,6 @@
 
 def find_peak(list_of_integers):
     """Finds the highest peak"""
-    if list_of_integers is None or list is []:
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
     return max(list_of_integers)
