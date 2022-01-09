cfaa853c68696ab1b94c3afaef393e22466378c9
Juanse1595 <3309@holbertonschool.com>
2022-01-09 23:54:38 +0000

    ADD 8th

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""8-json_api module"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        value = sys.argv[1]
+    else:
+        value = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data = {'q': value})
+    try:
+        msg = r.json()
+        if msg == {}:
+            print("No result")
+        else:
+            print("[{}] [{}]".format(msg['id'], msg['name']))
+    except requests.exceptions.HTTPError:
+        print("Not a valid JSON")
