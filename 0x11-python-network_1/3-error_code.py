1136694b546cf66077181577b1491443a17d34f7
Juanse1595 <3309@holbertonschool.com>
2022-01-09 19:55:37 +0000

    FIX 3rd adding if __name__

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,9 +5,10 @@ import urllib.error
 import sys
 
 
-req = urllib.request.Request(sys.argv[1])
-try:
-    response = urllib.request.urlopen(req)
-    print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
