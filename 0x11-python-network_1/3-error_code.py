6322f6dc9be1f539fd4638baecd3d4ab8cd4e179
Juanse1595 <3309@holbertonschool.com>
2022-01-09 19:54:10 +0000

    ADD 3rd

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """3-error_code module"""
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
-from sys import argv
+import urllib.request
+import urllib.error
+import sys
 
 
-req = Request(argv[1])
+req = urllib.request.Request(sys.argv[1])
 try:
-    response = urlopen(req)
+    response = urllib.request.urlopen(req)
     print(response.read().decode('utf-8'))
-except HTTPError as e:
+except urllib.error.HTTPError as e:
     print('Error code: {}'.format(e.code))
