61a8e7d4137c8c07cbca677c5e06dbb2fab541cd
Juanse1595 <3309@holbertonschool.com>
2022-01-09 15:27:24 +0000

    FIX code adding sys.argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.headers.get('X-Request-Id'))
