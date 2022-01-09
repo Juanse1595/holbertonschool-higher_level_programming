043b9b3c8c76e246c2299fdc0c2b56b1eba33083
Juanse1595 <3309@holbertonschool.com>
2022-01-09 21:12:34 +0000

    ADD 4rd

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     outp_str = r.content.decode('utf-8')
     print("Body response:\n\
-          \t- type: {}\n\
-          \t- content: {}".format(type(outp_str), outp_str))
+            - type: {}\n\
+            - content: {}".format(type(outp_str), outp_str))
