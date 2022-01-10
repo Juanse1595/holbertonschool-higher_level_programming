adea18c6bb9dd325ec0d8e12e4238935853db2c0
Juanse1595 <3309@holbertonschool.com>
2022-01-10 00:50:16 +0000

    FIX 10 adding passwd

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,11 @@ import requests
 
 if __name__ == '__main__':
     owner = sys.argv[1]
+    passwd = sys.argv[2]
     url = "https://api.github.com/users/{}".format(owner)
-    r = requests.get(url)
-    print(r.json()['id'])
+    headers = {'Authorization': 'token {}'.format(passwd)}
+    r = requests.get(url, headers=headers)
+    if r:
+        print(r.json()['id'])
+    else:
+        print('None')
