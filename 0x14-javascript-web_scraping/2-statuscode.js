2f00739c40c0b1861f68f51cb9eae006c61c2ca0
Juanse1595 <3309@holberstonschool.com>
2022-02-01 16:36:40 -0500

    FIX 2nd

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const myArgs = process.argv.slice(2);
 const url = myArgs[0];
 const request = require('request');
 request(url, (error, response, body) => {
-    console.log('code: ', response.statusCode);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code: ', response.statusCode);
 });
