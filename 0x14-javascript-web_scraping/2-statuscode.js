d563b8bde3e017683b414a8df84edd243dfaad7b
Juanse1595 <3309@holberstonschool.com>
2022-02-01 16:33:09 -0500

    ADD 2nd task file

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// myArgs retrieve the passed args, ignoring first 2
+const myArgs = process.argv.slice(2);
+const url = myArgs[0];
+const request = require('request');
+request(url, (error, response, body) => {
+    console.log('code: ', response.statusCode);
+});
